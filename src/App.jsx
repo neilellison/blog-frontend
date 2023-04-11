d61dfdd33fe2848d5342434d2689848e5e1054b0
NeilEllison <ellison.neil1@gmail.com>
2023-04-11 10:52:41 -0700

    updates, pictures working

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from "axios";
+import { useState } from "react";
 
 function Header() {
   return (
@@ -31,10 +32,10 @@ function PostsIndex(props) {
       <div id="posts-index">
         <h1>All posts</h1>
         {props.posts.map((post) =>(
-          div key={post.id} className="posts">
+          <div key={post.id} className="posts">
         <h2>{post.title} </h2>
       <img src={post.image} alt="this should have an image" />
-      <p>{post.body}
+      <p>{post.body}</p>
       </div>
         )
         ) }
@@ -64,29 +65,17 @@ function Footer() {
 }
   
 function Content() {
-  let posts = [
-    {
-      id: 1,
-      title: "Cloned multi-tasking extranet",
-      body: "Salvia humblebrag phlogiston pour-over. +1 locavore phlogiston. Ethical pitchfork venmo salvia shoreditch. Cold-pressed irony fashion axe gastropub. Raw denim letterpress yuccie single-origin coffee. Fashion axe echo put a bird on it kale chips aesthetic.",
-      image: "https://i.picsum.photos/id/302/200/300.jpg?hmac=b5e6gUSooYpWB3rLAPrDpnm8PsPb84p_NXRwD-DK-1I",
-    },
-    {
-      id: 2,
-      title: "Optional zero tolerance database",
-      body: "Muggle magic hashtag mixtape ramps celiac. Goth paleo echo small batch vhs semiotics. Five dollar toast butcher yolo salvia selvage cleanse farm-to-table. Viral carry disrupt salvia schlitz flannel. Truffaut normcore wayfarers salvia vinegar leggings phlogiston chicharrones. Locavore yolo kickstarter aesthetic flannel master. Green juice lumbersexual wolf williamsburg artisan seitan pbr&b. Yr sriracha raw denim taxidermy kale chips chartreuse tilde readymade.",
-      image: "https://i.picsum.photos/id/131/200/300.jpg?hmac=9q7mRSOguNBFGg_gPPRKlfjNINGjXWeDBTYPP1_gEas",
-    },
-    {
-      id: 3,
-      title: "Virtual zero administration productivity",
-      body: "Drinking craft beer before they sold out. Cardigan green juice distillery. Portland single-origin coffee pop-up. Kickstarter readymade shoreditch. Swag ennui banh mi cold-pressed chicharrones. Single-origin coffee yuccie selvage ethical polaroid next level stumptown. Master squid post-ironic. Yolo bushwick goth fixie forage actually pitchfork.",
-      image: "https://i.picsum.photos/id/852/200/300.jpg?hmac=6IMZOkPF_q5nf8IwfYdfxPUyKnyPL1w8moDjTeMOT5g",
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+  const handleIndexPosts = () => {
+    axios.get("http://localhost:3000/posts.json").then((response) => {
+      console.log(response.data);
+      setPosts(response.data);  
+    });
+  };
   return(
     <div>
       <PostsNew />
+      <button onClick={handleIndexPosts}>Load Posts</button>
       <PostsIndex posts={posts} />
     </div>
   );
